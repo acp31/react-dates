f80ca5e418c12a89f82c96902dda5dc973f4a119
Aaron Phillips <aphillips@johnmcneiltudio.com>
2017-03-22 15:43:44 -0700

    updated index.js to ES6

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
-var DateRangePicker = require('./lib/components/DateRangePicker').default;
-var DateRangePickerInput = require('./lib/components/DateRangePickerInput').default;
-var DateRangePickerInputController = require('./lib/components/DateRangePickerInputController').default;
-var SingleDatePicker = require('./lib/components/SingleDatePicker').default;
-var SingleDatePickerInput = require('./lib/components/SingleDatePickerInput').default;
-var DayPicker = require('./lib/components/DayPicker').default;
-var DayPickerRangeController = require('./lib/components/DayPickerRangeController').default;
-var CalendarMonthGrid = require('./lib/components/CalendarMonthGrid').default;
-var CalendarMonth = require('./lib/components/CalendarMonth').default;
-var CalendarDay = require('./lib/components/CalendarDay').default;
+const DateRangePicker = require('./lib/components/DateRangePicker').default;
+const DateRangePickerInput = require('./lib/components/DateRangePickerInput').default;
+const DateRangePickerInputController = require('./lib/components/DateRangePickerInputController').default;
+const SingleDatePicker = require('./lib/components/SingleDatePicker').default;
+const SingleDatePickerInput = require('./lib/components/SingleDatePickerInput').default;
+const DayPicker = require('./lib/components/DayPicker').default;
+const DayPickerRangeController = require('./lib/components/DayPickerRangeController').default;
+const CalendarMonthGrid = require('./lib/components/CalendarMonthGrid').default;
+const CalendarMonth = require('./lib/components/CalendarMonth').default;
+const CalendarDay = require('./lib/components/CalendarDay').default;
 
-var DateRangePickerShape = require('./lib/shapes/DateRangePickerShape').default;
-var SingleDatePickerShape = require('./lib/shapes/SingleDatePickerShape').default;
+const DateRangePickerShape = require('./lib/shapes/DateRangePickerShape').default;
+const SingleDatePickerShape = require('./lib/shapes/SingleDatePickerShape').default;
 
-var isInclusivelyAfterDay = require('./lib/utils/isInclusivelyAfterDay').default;
-var isInclusivelyBeforeDay = require('./lib/utils/isInclusivelyBeforeDay').default;
-var isNextDay = require('./lib/utils/isNextDay').default;
-var isSameDay = require('./lib/utils/isSameDay').default;
+const isInclusivelyAfterDay = require('./lib/utils/isInclusivelyAfterDay').default;
+const isInclusivelyBeforeDay = require('./lib/utils/isInclusivelyBeforeDay').default;
+const isNextDay = require('./lib/utils/isNextDay').default;
+const isSameDay = require('./lib/utils/isSameDay').default;
 
-var toISODateString = require('./lib/utils/toISODateString').default;
-var toLocalizedDateString = require('./lib/utils/toLocalizedDateString').default;
-var toMomentObject = require('./lib/utils/toMomentObject').default;
+const toISODateString = require('./lib/utils/toISODateString').default;
+const toLocalizedDateString = require('./lib/utils/toLocalizedDateString').default;
+const toMomentObject = require('./lib/utils/toMomentObject').default;
 
 
 module.exports = {
-  DateRangePicker: DateRangePicker,
-  SingleDatePicker: SingleDatePicker,
+  DateRangePicker,
+  SingleDatePicker,
+  DateRangePickerInputController,
+  DateRangePickerInput,
+  SingleDatePickerInput,
+  DayPicker,
+  DayPickerRangeController,
+  CalendarMonthGrid,
+  CalendarMonth,
+  CalendarDay,
 
-  DateRangePickerInputController: DateRangePickerInputController,
-  DateRangePickerInput: DateRangePickerInput,
-  SingleDatePickerInput: SingleDatePickerInput,
-  DayPicker: DayPicker,
-  DayPickerRangeController: DayPickerRangeController,
-  CalendarMonthGrid: CalendarMonthGrid,
-  CalendarMonth: CalendarMonth,
-  CalendarDay: CalendarDay,
+  DateRangePickerShape,
+  SingleDatePickerShape,
 
-  DateRangePickerShape: DateRangePickerShape,
-  SingleDatePickerShape: SingleDatePickerShape,
+  isInclusivelyAfterDay,
+  isInclusivelyBeforeDay,
+  isNextDay,
+  isSameDay,
 
-  isInclusivelyAfterDay: isInclusivelyAfterDay,
-  isInclusivelyBeforeDay: isInclusivelyBeforeDay,
-  isNextDay: isNextDay,
-  isSameDay: isSameDay,
-
-  toISODateString: toISODateString,
-  toLocalizedDateString: toLocalizedDateString,
-  toMomentObject: toMomentObject,
+  toISODateString,
+  toLocalizedDateString,
+  toMomentObject,
 };
