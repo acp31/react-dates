80b2824a8a829b4523536ace9a46b1fdd418bae0
Aaron Phillips <aphillips@johnmcneiltudio.com>
2017-03-22 15:14:29 -0700

    fixed export for constants

--- constants.js
+++ constants.js
@@ -1,14 +1,14 @@
-module.exports = {
-  DISPLAY_FORMAT: 'L',
-  ISO_FORMAT: 'YYYY-MM-DD',
 
-  START_DATE: 'startDate',
-  END_DATE: 'endDate',
+export const DISPLAY_FORMAT = 'L';
+export const ISO_FORMAT = 'YYYY-MM-DD';
 
-  HORIZONTAL_ORIENTATION: 'horizontal',
-  VERTICAL_ORIENTATION: 'vertical',
-  VERTICAL_SCROLLABLE: 'verticalScrollable',
+export const START_DATE = 'startDate';
+export const END_DATE = 'endDate';
 
-  ANCHOR_LEFT: 'left',
-  ANCHOR_RIGHT: 'right',
-};
+export const HORIZONTAL_ORIENTATION = 'horizontal';
+export const VERTICAL_ORIENTATION = 'vertical';
+export const VERTICAL_SCROLLABLE = 'verticalScrollable';
+
+export const ANCHOR_LEFT = 'left';
+export const ANCHOR_RIGHT = 'right';
+
